a40a48be5163815f395449bea1feacba3c4ebd42
barkha <barkhag03@gmail.com>
2026-02-25 13:41:37 +0100

    Added Exception classes

---
+++ user-service/src/main/java/com/flashkart/userService/exception/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.flashkart.userService.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
