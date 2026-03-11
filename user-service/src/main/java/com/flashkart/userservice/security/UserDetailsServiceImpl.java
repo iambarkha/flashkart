70d0b8ecda5c7a3d2c538e28a8d175a238f2a06c
barkha <barkhag03@gmail.com>
2026-03-11 16:59:27 +0100

    replaced spring's user with User

--- user-service/src/main/java/com/flashkart/userservice/security/UserDetailsServiceImpl.java
+++ user-service/src/main/java/com/flashkart/userservice/security/UserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package com.flashkart.userservice.security;
 
 import com.flashkart.userservice.entity.User;
 import com.flashkart.userservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,24 +13,14 @@ import org.springframework.stereotype.Service;
 import java.util.Collections;
 
 @Service
+@RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
 
-    public UserDetailsServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email)
+        return userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with Email: "+ email));
-        GrantedAuthority authority = new SimpleGrantedAuthority(user.getRole().name());
-        return new org.springframework.security.core.userdetails.User(
-                user.getEmail(),
-                user.getPassword(),
-                user.isEnabled(),
-                true, true, true,
-                Collections.singletonList(authority)
-        );
+
     }
 }
