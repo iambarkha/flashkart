3f20504fe32aaed53a677655b404aa7d12d61431
barkha <barkhag03@gmail.com>
2026-04-16 10:46:25 +0200

    removed unused imports

--- user-service/src/main/java/com/flashkart/userservice/exception/GlobalExceptionHandler.java
+++ user-service/src/main/java/com/flashkart/userservice/exception/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import com.flashkart.userservice.dto.response.ApiResponse;
 import io.jsonwebtoken.JwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
